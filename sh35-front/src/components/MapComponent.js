546f20dbc45bca4896ac60675dccde73a27f3228
Fin <2677288g@student.gla.ac.uk>
2023-10-18 13:06:59 +0100

    map upgrades - long & lat for mouse

--- sh35-front/src/components/MapComponent.js
+++ sh35-front/src/components/MapComponent.js
@@ -1,74 +1,114 @@
-// import React from 'react';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS for proper map styling
-
-
-// const defaultCenter = [40.7128, -74.0060]; // Latitude and Longitude for New York City
-// const defaultZoom = 13; // Default zoom level
-
-// const tileURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-
-// function MapComponent() {
-//     return (
-//         <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ width: '100%', height: '600px' }}>
-//             <TileLayer url={tileURL} />
-//             {/* You can also add a Marker to indicate a specific location */}
-//             <Marker position={defaultCenter}>
-//                 <Popup>
-//                     New York City
-//                 </Popup>
-//             </Marker>
-//         </MapContainer>
-//     );
-// }
-
-// export default MapComponent;
-
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
-import L from 'leaflet';
-import 'leaflet-draw';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-draw/dist/leaflet.draw.css';
+import React, { useState , useEffect } from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-draw";
+import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 
 const MapComponent = () => {
-    const defaultCenter = [40.7128, -74.0060]; // New York City coordinates
-    const defaultZoom = 13;
+  const defaultCenter = [55.869829854, -4.28583219]; // New York City coordinates
+  const defaultZoom = 10;
+  const [boundingBox, setBoundingBox] = useState(null);
+  const [mousePosition, setMousePosition] = useState(null);
 
-    const AddDrawControl = () => {
+    const MousePositionControl = () => {
         const map = useMap();
 
         useEffect(() => {
-            const drawControl = new L.Control.Draw({
-                draw: {
-                    rectangle: true,
-                    polyline: false,
-                    circle: false,
-                    circlemarker: false,
-                    marker: false,
-                    polygon: false
-                }
-            });
-            map.addControl(drawControl);
+            const updateMousePosition = (event) => {
+                setMousePosition({
+                    latlng: event.latlng,
+                    containerPoint: event.containerPoint
+                });
+            };
 
-            map.on(L.Draw.Event.CREATED, (e) => {
-                console.log("Shape created!", e);
-            });
+            map.on('mousemove', updateMousePosition);
 
             return () => {
-                map.off(L.Draw.Event.CREATED);
+                map.off('mousemove', updateMousePosition);
             };
         }, [map]);
 
         return null;
     };
 
-    return (
-        <MapContainer center={defaultCenter} zoom={defaultZoom} style={{ width: '100%', height: '600px' }}>
+
+  const AddDrawControl = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      if (map && !map.drawControl) {
+        const drawControl = new L.Control.Draw({
+          draw: {
+            rectangle: true,
+            polyline: false,
+            circle: false,
+            circlemarker: false,
+            marker: false,
+            polygon: false,
+          },
+        });
+        map.addControl(drawControl);
+        map.drawControl = drawControl;
+      }
+
+      map.on(L.Draw.Event.CREATED, (e) => {
+        const layer = e.layer;
+        if (e.layerType === 'rectangle') {
+            const bounds = layer.getBounds();
+            const southWest = bounds.getSouthWest(); // Bottom-left
+            const northEast = bounds.getNorthEast(); // Top-right
+            setBoundingBox({
+                bottomLeft: [southWest.lat, southWest.lng],
+                topRight: [northEast.lat, northEast.lng]
+            });
+        }
+        console.log("Shape created!", e);
+    });
+
+      return () => {
+        map.off(L.Draw.Event.CREATED);
+      };
+    }, [map]);
+
+    return null;
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+        <MapContainer
+            center={defaultCenter}
+            zoom={defaultZoom}
+            style={{ width: "100%", height: "600px" }}
+        >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <AddDrawControl />
+            <MousePositionControl /> {/* Add this line to include the MousePositionControl component */}
         </MapContainer>
-    );
-}
+        
+        {boundingBox && (
+            <div>
+                <p>Bottom Left: {boundingBox.bottomLeft.join(', ')}</p>
+                <p>Top Right: {boundingBox.topRight.join(', ')}</p>
+            </div>
+        )}
+        
+        {mousePosition && (
+            <div style={{
+                position: 'absolute',
+                top: mousePosition.containerPoint.y,
+                left: mousePosition.containerPoint.x,
+                backgroundColor: 'white',
+                padding: '5px',
+                border: '1px solid black',
+                pointerEvents: 'none',
+                zIndex: 1000
+            }}>
+                Lat: {mousePosition.latlng.lat.toFixed(4)}, Lng: {mousePosition.latlng.lng.toFixed(4)}
+            </div>
+        )}
+    </div>
+);
+};
 
 export default MapComponent;
