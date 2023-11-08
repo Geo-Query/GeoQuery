d112a0aff0da162b27e934ed3deca7f41130d5ba
ruariohara <ruari.ohara@gmail.com>
2023-11-08 17:00:01 +0000

    added bbox form and state to app

--- sh35-front/src/App.js
+++ sh35-front/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MapComponent from './components/MapComponent';
+import MapBoundingBoxForm from './api/FlaskEndpoints';
 
 function App() {
+    const [boundingBox, setBoundingBox] = useState(null);
+
     return (
         <div className="App">
-            <MapComponent />
+            <MapComponent setBoundingBox={setBoundingBox} boundingBox={boundingBox} />
+            <MapBoundingBoxForm boundingBox={boundingBox} />
         </div>
     );
 }
