66ed61b303e439c1f303a673cb66ca3af29519a2
ruariohara <ruari.ohara@gmail.com>
2024-01-10 19:22:01 +0000

    React frontend getting correct coords to post. Awaiting cors fix in backend

--- frontend/src/api/FlaskEndpoints.js
+++ frontend/src/api/FlaskEndpoints.js
@@ -1,23 +1,61 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from "axios";
-
-// Bottom Left: 55.830215663223996, -4.3157386779785165
-// Top Right: 55.88898321305664, -4.199352264404298
+import { RUST_BACKEND_URL } from '../config'; // Adjust the path as necessary
 
 const MapBoundingBoxForm = ({ boundingBox }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validateAndSanitizeData = (box) => {
+    // Check if the box object is provided and not empty
+    if (!box || Object.keys(box).length === 0) {
+      return { valid: false, message: "No bounding box provided" };
+    }
+
+    // Validate and sanitize longitude and latitude
+    const topLeftLong = parseFloat(box.bottomLeft.lng);
+    const topLeftLat = parseFloat(box.topRight.lat);
+    const bottomRightLong = parseFloat(box.topRight.lng);
+    const bottomRightLat = parseFloat(box.bottomLeft.lat);
+  
+    if ([topLeftLong, topLeftLat, bottomRightLong, bottomRightLat].some(isNaN)) {
+      console.log(topLeftLong, topLeftLat, bottomRightLong, bottomRightLat);
+      return { valid: false, message: "Invalid coordinate values" };
+    }
+    
+    if (topLeftLong < -180 || topLeftLong > 180 || bottomRightLong < -180 || bottomRightLong > 180) {
+      return { valid: false, message: "Longitude values must be between -180 and 180" };
+    }
+
+    if (topLeftLat < -90 || topLeftLat > 90 || bottomRightLat < -90 || bottomRightLat > 90) {
+      return { valid: false, message: "Latitude values must be between -90 and 90" };
+    }
+
+ 
+    return {
+      valid: true,
+      data: {
+        top_left_long: topLeftLong,
+        top_left_lat: topLeftLat,
+        bottom_right_long: bottomRightLong,
+        bottom_right_lat: bottomRightLat
+      }
+    };
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setErrorMessage(""); // Reset error message
     console.log(boundingBox);
-    if (!boundingBox) {
-      console.error("No bounding box to send");
+    const validationResult = validateAndSanitizeData(boundingBox);
+    if (!validationResult.valid) {
+      setErrorMessage(validationResult.message); // Set error message
       return;
     }
 
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/post-coordinates",
-        boundingBox
+        RUST_BACKEND_URL + "search",
+        validationResult.data
       );
       console.log(response.data);
     } catch (error) {
@@ -26,7 +64,12 @@ const MapBoundingBoxForm = ({ boundingBox }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex items-center">
+      {errorMessage && (
+        <div className="text-white font-bold py-2 px-4 mr-2">
+          {errorMessage}
+        </div>
+      )}
       <button
         className="export-btn bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-xl border-2 border-white"
         type="submit"
@@ -34,6 +77,7 @@ const MapBoundingBoxForm = ({ boundingBox }) => {
         Send Coordinates
       </button>
     </form>
+
   );
 };
 
