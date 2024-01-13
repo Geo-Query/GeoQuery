14d166d81de5461e499bf577d61b94c84dab20c9
Anwar Abdullah <2666539a@student.gla.ac.uk>
2024-01-13 21:35:00 +0000

    Fixed error related to collection of coordinates into a vector

--- backend/src/parsing/mbtiles.rs
+++ backend/src/parsing/mbtiles.rs
@@ -10,6 +10,7 @@ pub struct MBTilesRegion {
 }
 
 pub fn parse_mbtiles(reader: &mut BufReader<File>) -> Result<MBTilesRegion> {
+    println!("{}",reader);
     //Opens connection to MBTiles file(its just a sqlite db)
     let conn = Connection::open(reader).unwrap();
 
@@ -19,13 +20,19 @@ pub fn parse_mbtiles(reader: &mut BufReader<File>) -> Result<MBTilesRegion> {
     let mut rows = stmt.query([]).unwrap();
 
     while let Some(row) = rows.next().unwrap() {
-        let value: f64 = row.get("value").unwrap();
+        let value: String = row.get("value").unwrap();
 
         //Mbtiles bounds coordinates are stored as left, bottom, right, top
-        let values: Vec<&f64> = value.split(',').collect();
+        let values: Vec<f64> = value
+            .split(',')
+            .map(|s| s.trim().parse::<f64>())
+            .collect::<Result<_, _>>().unwrap();
+
+        //Goes lat then long
+        let bottom_left = (values[1], values[0]);
+        let top_right = (values[3], values[2]);
     }
 
-    let (bottom_left, top_right) = ((value[1],values[0]),(value[3],value[2]));
 
     return Ok(MBTilesRegion {
         bottom_left,
--- backend/src/parsing/mod.rs
+++ backend/src/parsing/mod.rs
@@ -1,3 +1,4 @@
 pub mod dt2;
 pub mod geojson;
 pub mod kml;
+pub mod mbtiles;
