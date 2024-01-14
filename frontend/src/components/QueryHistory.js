09e6dec407a2f02fd1ae8982711b0e0cdae80778
Calum Robertson <2665639r@student.gla.ac.uk>
2024-01-14 21:14:15 +0000

    Added Query onclick

--- frontend/src/components/QueryHistory.js
+++ frontend/src/components/QueryHistory.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./QueryHistory.css";
 
-const QueryHistory = ({queryHistory, setQueryHistory}) => {
-  // clears the list
+const QueryHistory = ({ queryHistory, setQueryHistory}) => {
+
+  // Function to clear the list of coordinates
   const clearCoordinates = () => {
-    // Clearing the coordinates list
     setQueryHistory([]);
   };
+
+  const handleCoordinateClick = () => {
+      alert("Clicked coordinates:");
+  }
 
   return (
     <div className="flex-grow p-6 border-2 border-white rounded-xl mx-6 my-2">
       <div className="bg-blue-600 text-white font-bold py-2 px-4 rounded-xl border-2 border-white min-w-full md:min-w-0 md:min-w-200px">
         <span className="font-bold">Query History</span>
       </div>
-      <div className= "log">
+      <div className="log">
         <ul>
           {queryHistory.map((query, index) => (
-            <li key={index}>
+            <li key={index} onClick={() => handleCoordinateClick(query)}>
               NW: {query.northWest.lat.toFixed(4)}, {query.northWest.lng.toFixed(4)},
               SE: {query.southEast.lat.toFixed(4)}, {query.southEast.lng.toFixed(4)}
             </li>
