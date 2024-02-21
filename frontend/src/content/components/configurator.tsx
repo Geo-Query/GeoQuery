a147d2ee01e4f038219e1eb3a2de27c9ec47b8c9
Calum Robertson <2665639r@student.gla.ac.uk>
2024-02-21 15:36:39 +0000

    Changed configurator.tsx to allow format conversion and validation

--- frontend/src/content/components/configurator.tsx
+++ frontend/src/content/components/configurator.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import SelectedRegion, {validateAndConformCoordinate} from "../utils/region";
+import React, { useState, useEffect } from "react";
+import SelectedRegion, { checkFormat, checkValid, validateAndConformCoordinate } from "../utils/region";
 import QueryHistory from "../utils/queryhistory";
 
 interface ConfiguratorProps {
@@ -8,28 +8,55 @@ interface ConfiguratorProps {
     queryHistory: QueryHistory,
     setQueryHistory: React.Dispatch<React.SetStateAction<QueryHistory>>
 }
+
 export default function Configurator(props: ConfiguratorProps) {
     const [northWestLat, setNWLat] = useState(undefined);
     const [northWestLong, setNWLong] = useState(undefined);
     const [southEastLat, setSELat] = useState(undefined);
     const [southEastLong, setSELong] = useState(undefined);
 
+    // Once populated, and every change after
     useEffect(() => {
-        if (northWestLat && northWestLong && southEastLong && southEastLat) {
-            props.setSelectedRegion({
-                region: {
-                    northWest: {
-                      lat: validateAndConformCoordinate(northWestLat),
-                      long: validateAndConformCoordinate(northWestLong)
-                    },
-                    southEast: {
-                        lat: validateAndConformCoordinate(southEastLat),
-                        long: validateAndConformCoordinate(southEastLong)
+
+        // validates present coordinates
+        if (northWestLat && northWestLong && southEastLat && southEastLong) {
+            console.log("Coords present");
+
+            // converts all formats to decimal degrees
+            const formattedNorthWestLat = checkFormat(northWestLat);
+            const formattedNorthWestLong = checkFormat(northWestLong);
+            const formattedSouthEastLat = checkFormat(southEastLat);
+            const formattedSouthEastLong = checkFormat(southEastLong);
+            console.log("Coords formatted");
+
+            const validNorthWestLat = checkValid("lat", formattedNorthWestLat);
+            const validNorthWestLong = checkValid("long", formattedNorthWestLong);
+            const validSouthEastLat = checkValid("lat", formattedSouthEastLat);
+            const validSouthEastLong = checkValid("long", formattedSouthEastLong);
+            console.log("Coords validated");
+
+            if(validNorthWestLat.isValid && validNorthWestLong.isValid && validSouthEastLat.isValid && validSouthEastLong.isValid){
+            console.log("Attempting to plot");
+                // check valid and convert formats
+                // if not valid 'grey out' make request button
+                // ACHIEVE BY SHARING SOME STATE BETWEEN REQUESTOR & CONFIGURATOR COMPONENT!
+                // ONLY SETSELECTEDREGION if valid.
+                props.setSelectedRegion({
+                    region: {
+                        northWest: {
+                            lat: validateAndConformCoordinate(validNorthWestLat.result),
+                            long: validateAndConformCoordinate(validNorthWestLong.result)
+                        },
+                        southEast: {
+                            lat: validateAndConformCoordinate(validSouthEastLat.result),
+                            long: validateAndConformCoordinate(validSouthEastLong.result)
+                        }
                     }
-                }
-            })
+                })
+            }
         }
     }, [northWestLat, northWestLong, southEastLat, southEastLong]);
+
 
     useEffect(() => {
         setNWLat(props.selectedRegion.region?.northWest.lat.toString());
@@ -38,10 +65,11 @@ export default function Configurator(props: ConfiguratorProps) {
         setSELong(props.selectedRegion.region?.southEast.long.toString());
     }, [props.selectedRegion]);
 
+    // every change
     return (
         <div className="flex flex-col my-2 text-white font-bold p-1 gap-4 text-left">
             <div className="flex items-center text-left gap-2">
-                <h1 style={{width: "15%"}}>North West: </h1>
+                <h1 style={{ width: "15%" }}>North West: </h1>
                 <div className="flex gap-3 w-full">
                     <input
                         type="number"
@@ -62,7 +90,7 @@ export default function Configurator(props: ConfiguratorProps) {
                 </div>
             </div>
             <div className="flex items-center text-left gap-2">
-                <h1 style={{width: "15%"}}>South East: </h1>
+                <h1 style={{ width: "15%" }}>South East: </h1>
                 <div className="flex gap-3 w-full">
 
                     <input
