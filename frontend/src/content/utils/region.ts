894a3c950034be121dc4ebf9cb9c7e0593f45036
Calum Robertson <2665639r@student.gla.ac.uk>
2024-02-21 15:09:04 +0000

    Added checkFormat()

--- frontend/src/content/utils/region.ts
+++ frontend/src/content/utils/region.ts
@@ -1,3 +1,6 @@
+import dmsToDecimal from "../components/DMS-DD";
+import dmdmToDecimal from "../components/DMDM-DD";
+
 export interface Coordinate {
     lat: number,
     long: number,
@@ -18,4 +21,23 @@ export default class SelectedRegion {
 
 export function validateAndConformCoordinate(initial: string): number {
     return parseFloat(initial);
+}
+
+export function checkFormat(initial: string): string {
+
+    const DMS = /^(\d{1,2} \d{1,2} \d{1,2}) ([NESW])$/;
+    const DMDM = /^(\d{1,2} \d{1,2}\.\d{4}) ([NESW])$/;
+
+    const isDMS = (DMS.test(initial));
+    const isDMDM = (DMDM.test(initial));
+
+    if(isDMS){
+        const digits = initial.split(" ");
+        return(dmsToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), parseFloat(digits[2]), digits[3]));
+    } else if (isDMDM){
+        const digits = initial.split(" ");
+        return(dmdmToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), digits[2]));
+    } else {
+        return initial;
+    }
 }
