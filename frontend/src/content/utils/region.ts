877fc2820548990dbc1d5849416eb84c860f4f84
Calum Robertson <2665639r@student.gla.ac.uk>
2024-02-28 12:36:22 +0000

    New validation system

--- frontend/src/content/utils/region.ts
+++ frontend/src/content/utils/region.ts
@@ -25,41 +25,60 @@ export function validateAndConformCoordinate(initial: string): number {
     return parseFloat(initial);
 }
 
-export function checkFormat(initial: string): number {
+export function convertAndOrValidate(initial: string): number {
 
+    const DD = /^\\d*\\.?\\d+$/;
     const DMS = /^(\d{1,2} \d{1,2} \d{1,2}) ([NESW])$/;
     const DMDM = /^(\d{1,2} \d{1,2}\.\d{4}) ([NESW])$/;
 
-    const isDMS = (DMS.test(initial));
-    const isDMDM = (DMDM.test(initial));
-
-    if(isDMS){
-        const digits = initial.split(" ");
-        return(dmsToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), parseFloat(digits[2]), digits[3]));
-    } else if (isDMDM){
-        const digits = initial.split(" ");
-        return(dmdmToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), digits[2]));
-    } else {
+    if(DMS.test(initial)){
+        const values = initial.split(" ");
+        return dmsToDecimal(parseInt(values[0]), parseInt(values[1]), parseInt(values[2]), values[3]);
+    } else if (DMDM.test(initial)){
+        const values = initial.split(" ");
+        return dmdmToDecimal(parseInt(values[0]), parseFloat(values[1]), values[2]);
+    } else if (DD.test(initial)) {
         return parseFloat(initial);
+    } else {
+        return 999; //if the box entry is not in either DD, DMS or DMDM return error code 999
     }
 }
 
-export function checkValid(coordType: 'lat' | 'long', initial:  number): { isValid: boolean; result?: number; error?: string} {
-
-    switch (coordType) {
-        case 'lat':
-            if (initial <= 90 && initial >= -90) {
-                return { isValid: true, result: initial};
-            } else {
-                return { isValid: false, error: 'Invalid Latitude'};
-            }
-        case 'long':
-            if (initial <= 180 && initial >= -180) {
-                return { isValid: true, result: initial };
-            } else {
-                return { isValid: false, error: 'Invalid Longitude' };
-            }
-        default:
-            return { isValid: false, error: 'Invalid coordinate type' };
-    }
-}
+// export function checkFormat(initial: string): number {
+//
+//     const DMS = /^(\d{1,2} \d{1,2} \d{1,2}) ([NESW])$/;
+//     const DMDM = /^(\d{1,2} \d{1,2}\.\d{4}) ([NESW])$/;
+//
+//     const isDMS = (DMS.test(initial));
+//     const isDMDM = (DMDM.test(initial));
+//
+//     if(isDMS){
+//         const digits = initial.split(" ");
+//         return(dmsToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), parseFloat(digits[2]), digits[3]));
+//     } else if (isDMDM){
+//         const digits = initial.split(" ");
+//         return(dmdmToDecimal(parseFloat(digits[0]), parseFloat(digits[1]), digits[2]));
+//     } else {
+//         return parseFloat(initial);
+//     }
+// }
+//
+// export function checkValid(coordType: 'lat' | 'long', initial:  number): { isValid: boolean; result?: number; error?: string} {
+//
+//     switch (coordType) {
+//         case 'lat':
+//             if (initial <= 90 && initial >= -90) {
+//                 return { isValid: true, result: initial};
+//             } else {
+//                 return { isValid: false, error: 'Invalid Latitude'};
+//             }
+//         case 'long':
+//             if (initial <= 180 && initial >= -180) {
+//                 return { isValid: true, result: initial };
+//             } else {
+//                 return { isValid: false, error: 'Invalid Longitude' };
+//             }
+//         default:
+//             return { isValid: false, error: 'Invalid coordinate type' };
+//     }
+// }
