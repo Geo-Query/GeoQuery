a573f1e0a21021cc7b8bebaa0687f74f7491d55d
Benjamin Parsons-Willis <2664758P@student.gla.ac.uk>
2024-03-14 23:59:49 +0000

    Added check for empty map dir

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 use std::path::{Path, PathBuf};
+use crate::error::RootErrorKind;
 
 pub fn read_path(configFile: File) -> Result<PathBuf ,Box<dyn Error>> {
     let mut path_str = String::new();
@@ -16,5 +17,8 @@ pub fn read_path(configFile: File) -> Result<PathBuf ,Box<dyn Error>> {
         }
     }
     path_str = path_str.replace("\"", "");
+    if path_str == "" {
+        return Err(RootErrorKind::InvalidMapDirectory("No directory specified! Define a map file directory in config.txt!".to_string()).into());
+    }
     return Ok(PathBuf::from(path_str));
 }
